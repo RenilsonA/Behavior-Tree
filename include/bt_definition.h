e4b58528f882e042cc410b58ed5e155e7d3858e9
Renilson <renilson.123@hotmail.com>
2024-06-30 13:27:38 -0300

    Add definition initial of behavior tree

---
+++ include/bt_definition.h
@@ -0,0 +1,78 @@
+/**
+ * @file bt_definition.h
+ * @author Renilson Almeida (renilson.123@hotmail.com)
+ * @brief Header of behavior tree definitions.
+ * @version 1.0
+ * @date 26/06/24
+ * 
+ * @copyright Copyright (c) 2024 Renilson Almeida
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef INCLUDE_BT_DEFINITION_H_
+#define INCLUDE_BT_DEFINITION_H_
+
+/**
+ * @brief Item inexistente.
+ *
+ */
+#define BT_DEFINITION_TREE_UNRELATED 255
+
+/**
+ * @brief Checagem de erro de uma função do tipo bt_definition_status_t.
+ *
+ */
+#define BT_DEFINITION_TREE_STATUS(_err)      \
+    do {                                        \
+        if (_err) {                             \
+            return BT_DEFINITION_STATUS_FAIL;\
+        }                                       \
+        return BT_DEFINITION_STATUS_SUCCESS; \
+    } while (0)
+
+/**
+ * @brief Enumera os tipos de retornos da árvore.
+ *
+ */
+typedef enum
+{
+    BT_DEFINITION_STATUS_SUCCESS = 0,     /**< Status da árvore parada. */                                      
+    BT_DEFINITION_STATUS_FAIL,            /**< Status da árvore executada com falha. */                             
+    BT_DEFINITION_STATUS_ERROR,           /**< Status da árvore montada com erro. */                             
+} bt_definition_status_t;
+
+/**
+ * @brief Enumera os tipos de nós da árvore.
+ *
+ */
+typedef enum
+{
+} bt_definition_node_type_t;
+
+/**
+ * @brief Estrutura de um nó da árvore.
+ *
+ */
+typedef struct __attribute__((__packed__)) bt_definition
+{
+      bt_definition_node_type_t node_type;                  /**< Tipo de nó. */
+} bt_definition_t;
+
+#endif /* INCLUDE_BT_DEFINITION_H_ */
